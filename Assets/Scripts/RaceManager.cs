0736c47b64237ee136c312d9e694e014d78c20dd
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-01 16:22:46 -0800

    got first iteration of spawner working?

--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -11,10 +11,37 @@ public class RaceManager : MonoBehaviour
     };
 
     // Keep array of all active players in the race
+    private GameObject[] playerList;
+
+    [Header("Player Spawner System")]
+    // Number of players per row
+    [SerializeField]
+    [Tooltip("Amount of players per row")]
+    private int playersPerRow = 2;
+
+    // Gap between size
+    [SerializeField]
+    [Tooltip("Gap between player")]
+    private float gap = 1f;
+
+    // Starting Vector
+    [SerializeField]
+    [Tooltip("Starting position of first player")]
+    private Vector3 startingPlayer;
+
+    // Which XYZ positions are you using
+    [SerializeField]
+    [Tooltip("If True, lines up players by Z axis, else lines up by X axis")]
+    private bool lineUpOnZ;
 
     private void Awake()
     {
+        // Get all active players
+        playerList = GameObject.FindGameObjectsWithTag("Player");
+
         // Freeze player movement and teleport them to starting positions
+        // Loop through players Spawn player each row and column to fit in system
+        RepositionPlayers();
     }
 
     void Start()
@@ -27,6 +54,33 @@ public class RaceManager : MonoBehaviour
         StartCountdown();
 
         // Make a timer
+    }
+
+    private void RepositionPlayers() 
+    {
+        int row = 0;
+        int col = 0;
+
+        // Takes list of players and repositions them to a specific point on spawn
+        for (int i = 0; i < playerList.Length; i++)
+        {
+            GameObject currPlayer = playerList[i];
+
+            Vector3 currPos = startingPlayer;
+
+            if (lineUpOnZ)
+            {
+                Vector3 currSpace = new Vector3(row * gap, 0, col * gap);
+                currPos += currSpace;
+            }
+            else 
+            {
+                Vector3 currSpace = new Vector3(col * gap, 0, row * gap);
+                currPos += currSpace;
+            }
+
+            currPlayer.transform.position = currPos;
+        }
     }
 
     // Possibly a couroutine?
