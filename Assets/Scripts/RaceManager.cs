6664cf0b3e73bdadc475bf9c54f84b7a5c346e67
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-16 18:19:35 -0800

    gameplay loop should be finished

--- Assets/Scripts/RaceManager.cs
+++ Assets/Scripts/RaceManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RaceManager : NetworkBehaviour
 {
     public struct PlayerStats
     {
+        public string player;
         public float raceTime;
+        public string raceTimeFormatted;
     };
+
+    // Queue for race finishes
+    public Queue<PlayerStats> raceResults = new Queue<PlayerStats>();
+
+    [SerializeField]
+    [Tooltip("Stopwatch for the race")]
+    private StopwatchController swControl;
 
     // Keep array of all active players in the race
     private NetworkObject[] networkObjList;
@@ -25,15 +35,12 @@ public class RaceManager : NetworkBehaviour
     [Tooltip("Gap between player")]
     private float gap = 1f;
 
-    // Starting Vector
+    // Starting Location for player
+    // can change transform to vector3 if you want a very specific point
+    // Otherwise just drag empty game object
     [SerializeField]
     [Tooltip("Gameobject of start point")]
     private Transform startingPoint;
-
-    // Starting Vector
-    [SerializeField]
-    [Tooltip("Starting position of first player")]
-    private Vector3 startingPlayer;
 
     // Which XYZ positions are you using
     [SerializeField]
@@ -44,28 +51,9 @@ public class RaceManager : NetworkBehaviour
     [Tooltip("Countdown till race starts")]
     private float countdown = 3f;
 
-    private bool raceStarted = false;
-    private bool raceEnded = false;
-
     private void Awake()
     {
         playerList = new List<GameObject>();
-        // if (IsServer)
-        // {
-        //     Debug.Log("IS in server");
-
-        //     playerList = FindObjectsOfType<NetworkObject>();
-
-        //     RepositionPlayers();
-        // }
-
-
-        // // Get all active players
-        // playerList = GameObject.FindGameObjectsWithTag("Player");
-
-        // // Freeze player movement and teleport them to starting positions
-        // // Loop through players Spawn player each row and column to fit in system
-        // RepositionPlayers();
     }
 
     void Start()
@@ -75,23 +63,13 @@ public class RaceManager : NetworkBehaviour
         {
             Debug.Log("IS in server");
 
+            // Network objects are not players, players are a separate list
             networkObjList = FindObjectsOfType<NetworkObject>();
 
-            // Moves camera with player but not the player itself
             RepositionPlayers();
 
             StartCoroutine(StartCountdown());
         }
-    }
-    void Update()
-    {
-        // if (!raceStarted) 
-        // {
-        //     StartCoroutine(StartCountdown());
-        // }
-        // Check if all players are loaded into scene
-
-        // Make a timer
     }
 
     private void RepositionPlayers() 
@@ -102,7 +80,7 @@ public class RaceManager : NetworkBehaviour
         // Takes list of players and repositions them to a specific point on spawn
         for (int i = 0; i < networkObjList.Length; i++)
         {
-
+            // Finds the network objects that are players
             Transform playerTransform = networkObjList[i].transform.Find("Player");
 
             if (playerTransform != null)
@@ -112,6 +90,7 @@ public class RaceManager : NetworkBehaviour
                 // Vector3 currPos = startingPlayer;
                 Vector3 currPos = startingPoint.position;
 
+                // Lines up players on X or Z axis
                 if (lineUpOnZ)
                 {
                     Vector3 currSpace = new Vector3(row * gap, 0, col * gap);
@@ -135,7 +114,6 @@ public class RaceManager : NetworkBehaviour
             {
                 // Handle the case when the Player object doesn't exist
                 Debug.LogWarning("Player object not found for playerList[" + i + "]");
-                // You can add more error handling or skip the iteration as needed
             }
             
         }
@@ -151,24 +129,98 @@ public class RaceManager : NetworkBehaviour
         yield return new WaitForSeconds(countdown);
 
         Debug.Log("Timer has finished");
-        raceStarted = true;
 
         for (int i = 0; i < playerList.Count; i++)
         {
             GameObject currPlayer = playerList[i];
             currPlayer.GetComponent<PlayerStateMachine>().enabled = true;
         }
-    }
 
-    // Call when player reaches end of map (collider/trigger)
-    private void PlayerFinishRace()
-    {
-        // Get time of player when they finish
+        swControl.StartStopwatch();
     }
 
     private void EndRace()
     {
-        // Determine player with the lowest time as the winner
+        Debug.Log("Finished Race");
+        swControl.StopStopwatch();
+
+        // Just dequeue the players to rank each one
+        int rank = 1;
+        while (raceResults.Count > 0)
+        {
+            PlayerStats playerstats = raceResults.Dequeue();
+            Debug.Log(string.Format("Rank {0}: {1} {2}", rank, playerstats.player, playerstats.raceTimeFormatted));
+        }
     }
+
+    // On colliding with finish line
+    private void OnTriggerEnter( Collider col ) 
+    {
+        Debug.Log("Touched");
+        PlayerStats playerStats = new PlayerStats();
+
+        // Saves player object name
+        playerStats.player = col.gameObject.name;
+
+        playerStats.raceTime = swControl.GetStopwatchTime();
+
+        // Saves time of player object
+        playerStats.raceTimeFormatted = swControl.FormatTime(playerStats.raceTime);
+
+        raceResults.Enqueue(playerStats);
+
+        
+
+        // Prints all the results in the console.
+        Debug.Log(raceResults);
+        Debug.Log(raceResults.Peek().player);
+        Debug.Log(raceResults.Peek().raceTime);
+        Debug.Log(raceResults.Peek().raceTimeFormatted);
+
+        if (raceResults.Count >= playerList.Count)
+        {
+            EndRace();
+        }
+
+        // if (col.CompareTag("Player"))
+        // {
+            
+        // }
+    }
+
+    // Call when player reaches end of map (collider/trigger)
+    // private void PlayerFinishRace()
+    // {
+    //     // Get time of player when they finish
+    // }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green; // Set Gizmos color
+
+        if (startingPoint != null)
+        {
+            Gizmos.DrawWireCube(startingPoint.position, new Vector3(playersPerRow * gap, 1f, playersPerRow * gap));
+        }
+
+        Gizmos.color = Color.red; // End point
+        
+        BoxCollider boxCollider = GetComponent<BoxCollider>(); 
+        if (boxCollider != null)
+        {
+            Gizmos.DrawCube(transform.position, boxCollider.size);
+            Gizmos.DrawWireCube(transform.position, boxCollider.size);
+        }
+    }
+
+    // If player tries to exit the field, they reset to center of finish line
+    // Bug where camera disconnects from player position
+    // private void OnTriggerExit( Collider col )
+    // {
+    //     Debug.Log("Exit Attempt");
+    //     BoxCollider boxCollider = GetComponent<BoxCollider>(); 
+
+    //     col.gameObject.transform.position = transform.position;
+    // }
 
 }
